1fa97559b83cfec9c98b9e00a2c3cfc6034bb496
Samir-Sell <67107744+Samir-Sell@users.noreply.github.com>
2022-06-20 11:44:03 -0400

    Update error.py

--- error.py
+++ error.py
@@ -1,5 +1,11 @@
-def error(e):
+import os
+import sys
+
+def error(e, kill):
     print(f"Error: {e}")
     exc_type, exc_obj, exc_tb = sys.exc_info()
     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
     print(f"Error Type: {exc_type}, File Name: {fname}, Line Number: {exc_tb.tb_lineno}")
+    if kill == True:
+        print("Exiting Program")
+        sys.exit
